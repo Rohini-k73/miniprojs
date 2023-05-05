2a2c9a246e6c4ec046b8888a06c73fc05da63a8a
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-05-05 11:34:06 +0530

    complete practice ques
    practice

---
+++ js_journey/4.practiceset2/script.js
@@ -0,0 +1,75 @@
+// Create a JavaScript program to check if a age is between 10 and 20.
+
+// In this example, we use the && operator to check if the age is between 10 and 20. If the age is between 10 and 20, the condition evaluates to true, and the first console.log statement is executed. If the age is not between 10 and 20, the condition evaluates to false, and the second console.log statement is executed.
+
+// let age = "what is your age"
+
+// if(age>=10 && age<=20){
+//     console.log("your age lie btn 10 or 20")
+// }else{
+    
+//     console.log("your age doesnot lie btn 10 or 20")
+// }
+
+
+//  ---------------------------------------------------------------------------------------------------------------------------------
+
+
+// Q2: Using Switch Case
+
+// let age = prompt ("what is your age")
+// switch(age){
+//     case '12':
+//         console.log("your age is 12")
+//         break;
+//     case '13':
+//         console.log("your age is 13")
+//         break;
+//     case '16':
+//         console.log("your age is 16")
+//         break;
+//     case '21':
+//         console.log("your age is 21")
+//         break;
+//         default :
+//         console.log ("your age is not valid")
+        
+// }
+
+// ----------------------------------------------------------------------------------
+
+// Q3: Divisible by 2 and 3
+
+// let num = promt("enter your num")
+// num= Number.parseInt(num)
+
+// if(num%2==0 && num%3==0){
+//     console.log("your num is divisible by 2 and 3")
+
+// }else{
+//     console.log("your num is divisible by 2 and 3")
+
+// }
+
+
+// ------------------------------------------
+
+// Q4: Divisible by 2 or 3
+
+/*let num = promt("enter your num")
+num= Number.parseInt(num)
+
+if(num%2==0 || num%3==0){
+    console.log("your num is divisible by 2 or 3")
+
+}else{
+    console.log("your num is divisible by 2 or 3")
+
+}*/ 
+
+// ---------------------------------------------------
+
+// Q5: Using Ternary Operator
+
+let age = prompt("Enter your age");
+age >= 18 ? console.log("You can drive") : console.log("You cannot drive");
